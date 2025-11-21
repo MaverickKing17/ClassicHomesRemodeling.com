383e4dbcc455182dd0d1f26db3f745ec36f90245
MaverickKing17 <kingnarmer702@gmail.com>
2025-11-21 15:23:15 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+// vite.config.ts
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,6 +7,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ✅ ADD THIS LINE
+      base: '/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
