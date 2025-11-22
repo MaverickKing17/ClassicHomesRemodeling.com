1816b81c13c0c61bb8f1fbb335adcfd2caf2c790
MaverickKing17 <kingnarmer702@gmail.com>
2025-11-22 14:11:01 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
+import App from './App';
+import SkipLink from '@/components/SkipLink';
+import TawkToChat from '@/components/TawkToChat';
 
+export default function Root() {
+  return (
+    <>
+      <SkipLink />
+      <App />
+      <TawkToChat />
+    </>
+  );
+}
